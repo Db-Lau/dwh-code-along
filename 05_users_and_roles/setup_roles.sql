00be7797435f08292b799aa612493d7b56b13c02
Db-Lau <debbielau.hk@gmail.com>
2026-09-16 13:41:35 +0200

    add access control - ice cream ex.

--- 05_users_and_roles/setup_roles.sql
+++ 05_users_and_roles/setup_roles.sql
@@ -14,3 +14,20 @@ GRANT USAGE ON DATABASE ice_cream_db TO ROLE ice_cream_reader;
 GRANT USAGE ON ALL SCHEMAS IN DATABASE ice_cream_db TO ROLE ice_cream_reader;
 GRANT SELECT ON ALL TABLES IN SCHEMA ice_cream_db.public TO ROLE ice_cream_reader;
 GRANT SELECT ON FUTURE TABLES IN SCHEMA ice_cream_db.public TO ROLE ice_cream_reader;
+
+SHOW GRANTS TO ROLE ice_cream_reader;
+
+--manage grants for the role ice_cream_write
+GRANT ROLE ice_cream_reader TO ROLE ice_cream_writer;
+
+
+GRANT INSERT,
+UPDATE,
+DELETE ON ALL TABLES IN SCHEMA ice_cream_db.public TO ROLE ice_cream_writer;
+GRANT INSERT,
+UPDATE,
+DELETE ON FUTURE TABLES IN SCHEMA ice_cream_db.public TO ROLE ice_cream_writer;
+
+GRANT CREATE TABLE ON SCHEMA ice_cream_db.public TO ROLE ice_cream_writer;
+
+GRANT ROLE ice_cream_writer TO USER dlau;
